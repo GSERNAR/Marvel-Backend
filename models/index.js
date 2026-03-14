2be0cb2db11bfb4ec360280641f9922ff409333b
GSERNAR <santisernaramirez@gmail.com>
2026-03-14 17:00:18 -0500

    index with modules

--- models/index.js
+++ models/index.js
@@ -4,12 +4,14 @@ const { formsModel } = require('./nosql/forms')
 const { powersModel } = require('./nosql/powers')
 const { attributeModel } = require('./nosql/attributes')
 const { attributeValuesModel, attributeStacksModel } = require('./nosql/attributeStacks')
+const { ModulesModel } = require('./nosql/modules')
 
 module.exports = {
     usersModel,
     charactersModel,
     formsModel,
     powersModel,
+    ModulesModel,
     attributeModel,
     attributeValuesModel,
     attributeStacksModel
